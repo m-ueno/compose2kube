c4a8f626f57ef3b390cbaa64480c26042d87e33d
UENO, Masaru <ueno.masaru@fujitsu.com>
2024-02-15 06:26:57 +0000

    Refactor main

--- src/compose2kube/__main__.py
+++ src/compose2kube/__main__.py
@@ -1,72 +1,36 @@
-import os
-from pathlib import Path
-import glob
 import argparse
+import glob
+import logging
+import os
 import pickle
 from logging import getLogger
-import logging
+from pathlib import Path
+import sys
 from typing import Optional
 
 import langfuse
-from langfuse.callback import CallbackHandler
 from langchain.globals import set_debug, set_verbose
+from langfuse.callback import CallbackHandler
 
 from compose2kube import evaluator
 
-
-def setup_logger(namespace: str, level: int):
-    _root = getLogger(namespace)
-    _root.setLevel(level)
-    handler = logging.StreamHandler()
-    handler.setLevel(level)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler.setFormatter(formatter)
-    _root.addHandler(handler)
-
-
-parser = argparse.ArgumentParser(prog="compose2kube")
-parser.add_argument(
-    "--convert",
-    action="store_true",
-    help="run convert compose.yaml to kube.yaml chain",
-)
-parser.add_argument("--eval", action="store_true", help="run evaluate chain")
-parser.add_argument("--verbose", "-v", action="store_true")
-parser.add_argument("--debug", action="store_true")
-parser.add_argument("--sessionid", "--sid", type=str, default="c2ksession")
-args = parser.parse_args()
-if not (args.convert or args.eval):
-    parser.print_help()
-    exit(1)
 
 PKGROOT = Path(os.path.dirname(os.path.abspath(__file__)))
 INPUTROOTDIR = PKGROOT.parent.parent / "data" / "deployments_anonymized"
 HUMANROOTDIR = PKGROOT.parent.parent / "data" / "manifest-by-human"
 TMPFILE = "/tmp/got.pkl"
 EVALFILE = "/tmp/eval.pkl"
-
-loglevel = logging.ERROR
-if args.verbose:
-    loglevel = logging.INFO
-elif args.debug:
-    loglevel = logging.DEBUG
-
-setup_logger("compose2kube", loglevel)
-setup_logger(__name__, loglevel)
-set_verbose(args.verbose)
-set_debug(args.debug)
+SESSON_ID = None
 logger = getLogger(__name__)
-langfuse_client = langfuse.Langfuse()
-if not langfuse_client.auth_check():
-    raise RuntimeError("langfuse auth failed")
-SESSION_ID = args.sessionid
 
 
 def get_handler(
     trace_name: str, user_id: str, session_id: Optional[str] = None
 ) -> CallbackHandler:
+    langfuse_client = langfuse.Langfuse()
+    if not langfuse_client.auth_check():
+        raise RuntimeError("langfuse auth failed")
+
     sess = {}
     if session_id:
         sess = {"session_id": session_id}
@@ -77,13 +41,13 @@ def get_handler(
     return handler
 
 
-def convert():
+def convert(session_id):
     inputfiles = glob.glob(f"{INPUTROOTDIR}/*/compose.yaml")
     answerfiles = glob.glob(f"{HUMANROOTDIR}/*/all.yaml")
     input = [{"input": k, "answer": v} for k, v in zip(inputfiles, answerfiles)]
     chain = evaluator.convert_chain
     handler = get_handler(
-        trace_name="compose2kube:convert", user_id=__name__, session_id=SESSION_ID
+        trace_name="compose2kube:convert", user_id=__name__, session_id=session_id
     )
 
     got = chain.batch(input, config={"callbacks": [handler]})
@@ -95,7 +59,7 @@ def convert():
     return got
 
 
-def evaluate():
+def evaluate(session_id):
     with open(TMPFILE, "rb") as f:
         got = pickle.load(f)
 
@@ -104,7 +68,7 @@ def evaluate():
 
     chain = evaluator.eval_chain
     handler = get_handler(
-        trace_name="compose2kube:evaluate", user_id=__name__, session_id=SESSION_ID
+        trace_name="compose2kube:evaluate", user_id=__name__, session_id=session_id
     )
     got2 = chain.batch(items, config={"callbacks": [handler]})
     with open(EVALFILE, "wb") as f:
@@ -112,9 +76,51 @@ def evaluate():
     logger.info(f"wrote {EVALFILE}")
 
 
-if args.convert:
-    logger.info("start convert")
-    convert()
-if args.eval:
-    logger.info("start eval")
-    evaluate()
+def main():
+    def setup_logger(namespace: str, level: int):
+        _root = getLogger(namespace)
+        _root.setLevel(level)
+        handler = logging.StreamHandler()
+        handler.setLevel(level)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        _root.addHandler(handler)
+
+    parser = argparse.ArgumentParser(prog="compose2kube")
+    parser.add_argument(
+        "--convert",
+        action="store_true",
+        help="run convert compose.yaml to kube.yaml chain",
+    )
+    parser.add_argument("--eval", action="store_true", help="run evaluate chain")
+    parser.add_argument("--verbose", "-v", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--sessionid", "--sid", type=str, default="c2ksession")
+    args = parser.parse_args()
+    if not (args.convert or args.eval):
+        parser.print_help()
+        exit(1)
+
+    loglevel = logging.ERROR
+    if args.verbose:
+        loglevel = logging.INFO
+    elif args.debug:
+        loglevel = logging.DEBUG
+
+    setup_logger("compose2kube", loglevel)
+    setup_logger(__name__, loglevel)
+    set_verbose(args.verbose)
+    set_debug(args.debug)
+
+    if args.convert:
+        logger.info("start convert")
+        convert(args.sessionid)
+    if args.eval:
+        logger.info("start eval")
+        evaluate(args.sessionid)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
