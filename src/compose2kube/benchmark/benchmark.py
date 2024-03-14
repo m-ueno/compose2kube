88da758b3664931a5bc73204ba2baf9e8f0fbfc5
UENO, Masaru <ueno.masaru@fujitsu.com>
2024-03-14 16:40:29 +0900

    Use @catch_decorator: 元の挙動は変えないように選択的にデコレート

--- src/compose2kube/benchmark/benchmark.py
+++ src/compose2kube/benchmark/benchmark.py
@@ -249,6 +249,7 @@ def catch_decorator(fn):
     return wrapped_fn
 
 
+@catch_decorator
 def judge3(manifests_str: str) -> Judgement:
     try:
         manifests = yaml.safe_load_all(manifests_str)
@@ -277,36 +278,35 @@ def judge4_easy(manifests: str) -> Judgement:
     return Judgement(ok=False, metadata={"message": "expected image string not found"})
 
 
+@catch_decorator
 def judge4(manifests: str) -> Judgement:
-    try:
-        documents = yaml.safe_load_all(manifests)
-        for doc in documents:
-            if doc is None:
+    documents = yaml.safe_load_all(manifests)
+    for doc in documents:
+        if doc is None:
+            continue
+        match doc.get("kind", ""):
+            case "Deployment" | "StatefulSet":
+                containers = (
+                    doc.get("spec", {})
+                    .get("template", {})
+                    .get("spec", {})
+                    .get("containers", [])
+                )
+
+            case "Pod":
+                containers = doc.get("spec", {}).get("containers", {})
+            case _:
                 continue
-            match doc.get("kind", ""):
-                case "Deployment" | "StatefulSet":
-                    containers = (
-                        doc.get("spec", {})
-                        .get("template", {})
-                        .get("spec", {})
-                        .get("containers", [])
-                    )
+        for container in containers:
+            image = container.get("image", "")
+            env_vars = container.get("env", [])
+            if image == "$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_VERSION":
+                return Judgement(ok=True, metadata={"env": env_vars})
 
-                case "Pod":
-                    containers = doc.get("spec", {}).get("containers", {})
-                case _:
-                    continue
-            for container in containers:
-                image = container.get("image", "")
-                env_vars = container.get("env", [])
-                if image == "$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_VERSION":
-                    return Judgement(ok=True, metadata={"env": env_vars})
-
-        return Judgement(ok=False, metadata={"message": "Incorrect image"})
-    except Exception as e:
-        return Judgement(ok=False, metadata={"error": str(e)})
+    return Judgement(ok=False, metadata={"message": "Incorrect image"})
 
 
+@catch_decorator
 def judge5(manifests: str) -> Judgement:
     def is_valid_dns_name(name: str) -> bool:
         """Check if the given string is a valid DNS name."""
@@ -314,71 +314,66 @@ def judge5(manifests: str) -> Judgement:
             return True
         return False
 
-    try:
-        documents = yaml.safe_load_all(manifests)
+    documents = yaml.safe_load_all(manifests)
 
-        at_least_one_service_name_is_valid = False
-        for doc in documents:
-            if doc is None:
-                continue
-            if not isinstance(doc, dict):
-                raise ValueError(f"YAML parsed non dict: {doc}")
-            if doc.get("kind", "") == "Service":
-                metadata_name = doc.get("metadata", {}).get("name", "")
-                if is_valid_dns_name(metadata_name):
-                    at_least_one_service_name_is_valid = True
-                else:
-                    return Judgement(
-                        ok=False,
-                        metadata={"message": f"Invalid DNS name: {metadata_name}"},
-                    )
-        return Judgement(
-            ok=at_least_one_service_name_is_valid,
-            metadata={
-                "message": "At least one service name is valid"
-                if at_least_one_service_name_is_valid
-                else "No service found"
-            },
-        )
-    except Exception as e:
-        return Judgement(ok=False, metadata={"error": e})
+    at_least_one_service_name_is_valid = False
+    for doc in documents:
+        if doc is None:
+            continue
+        if not isinstance(doc, dict):
+            raise ValueError(f"YAML parsed non dict: {doc}")
+        if doc.get("kind", "") == "Service":
+            metadata_name = doc.get("metadata", {}).get("name", "")
+            if is_valid_dns_name(metadata_name):
+                at_least_one_service_name_is_valid = True
+            else:
+                return Judgement(
+                    ok=False,
+                    metadata={"message": f"Invalid DNS name: {metadata_name}"},
+                )
+    return Judgement(
+        ok=at_least_one_service_name_is_valid,
+        metadata={
+            "message": "At least one service name is valid"
+            if at_least_one_service_name_is_valid
+            else "No service found"
+        },
+    )
 
 
+@catch_decorator
 def judge12(manifests: str) -> Judgement:
-    try:
-        documents = yaml.safe_load_all(manifests)
-        for doc in documents:
-            if doc is None:
+    documents = yaml.safe_load_all(manifests)
+    for doc in documents:
+        if doc is None:
+            continue
+        match doc.get("kind"):
+            case "Deployment" | "StatefulSet":
+                containers = (
+                    doc.get("spec", {})
+                    .get("template", {})
+                    .get("spec", {})
+                    .get("containers", [])
+                )
+            case "Pod":
+                containers = doc.get("spec", {}).get("containers", [])
+            case _:
                 continue
-            match doc.get("kind"):
-                case "Deployment" | "StatefulSet":
-                    containers = (
-                        doc.get("spec", {})
-                        .get("template", {})
-                        .get("spec", {})
-                        .get("containers", [])
+        for container in containers:
+            for probe_type in ["livenessProbe", "readinessProbe"]:
+                probe = container.get(probe_type, {})
+                if probe.get("httpGet", {}).get("path", "") == "/jupyter/lab":
+                    return Judgement(
+                        ok=True,
+                        metadata=dict(probe=probe),
                     )
-                case "Pod":
-                    containers = doc.get("spec", {}).get("containers", [])
-                case _:
-                    continue
-            for container in containers:
-                for probe_type in ["livenessProbe", "readinessProbe"]:
-                    probe = container.get(probe_type, {})
-                    if probe.get("httpGet", {}).get("path", "") == "/jupyter/lab":
-                        return Judgement(
-                            ok=True,
-                            metadata=dict(probe=probe),
-                        )
-        return Judgement(
-            ok=False,
-            metadata={
-                "message": "No probe with /jupyter/lab found",
-                "probe": probe if "probe" in locals() else None,
-            },
-        )
-    except Exception as e:
-        return Judgement(ok=False, metadata={"error": str(e)})
+    return Judgement(
+        ok=False,
+        metadata={
+            "message": "No probe with /jupyter/lab found",
+            "probe": probe if "probe" in locals() else None,
+        },
+    )
 
 
 def judge9(manifests: str) -> Judgement:
